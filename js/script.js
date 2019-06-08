19db59ce777a3a9da7f77b8aecfe46c6e11da522
Anton Serdyuchenko <anton415@gmail.com>
2019-06-08 13:36:37 +0300

    Add ”T-Shirt Info” section.

--- js/script.js
+++ js/script.js
@@ -17,3 +17,35 @@ $jobRoleTitle.change(() => {
     $textboxForJobRoleTitle.hide();
   }
 });
+
+/*
+  For the T-Shirt "Color" menu, only display the color options that match the
+  design selected in the "Design" menu.
+*/
+const $designSelect = $('#design');
+const $colorSelect = $('#color');
+
+$colorSelect.empty().append(
+  $('<option></option>').attr('value', 'default').text('First, Select Desig Theme')
+);
+
+$designSelect.change(() => {
+  console.log($designSelect.val());
+  if ($designSelect.val() == 'js puns') {
+    $colorSelect.empty().append(
+      $('<option></option>').attr('value', 'cornflowerblue').text('Cornflower Blue'),
+      $('<option></option>').attr('value', 'darkslategrey').text('Dark Slate Grey'),
+      $('<option></option>').attr('value', 'gold').text('Gold')
+    );
+  } else if ($designSelect.val() == 'heart js') {
+    $colorSelect.empty().append(
+      $('<option></option>').attr('value', 'tomato').text('Tomato'),
+      $('<option></option>').attr('value', 'steelblue').text('Steel Blue'),
+      $('<option></option>').attr('value', 'dimgrey').text('Dim Grey')
+    );
+  } else {
+    $colorSelect.empty().append(
+      $('<option></option>').attr('value', 'default').text('First, Select Desig Theme')
+    );
+  }
+});
