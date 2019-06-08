4536a0d753f4b258bf2a41df9710d97efd083b0d
Anton Serdyuchenko <anton415@gmail.com>
2019-06-08 12:08:34 +0300

    Add ”Job Role” section.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 // When the page first loads, the first text field should be in focus by default.
 $('#name').focus();
+
+/*
+  Include a text field that will be revealed when the "Other" option is
+  selected from the "Job Role" drop down menu.
+*/
+const $jobRoleTitle = $('#title');
+const $textboxForJobRoleTitle = $("<input type=text id=other-title placeholder='Your Job Role' style='display:none'>");
+
+$jobRoleTitle.after($textboxForJobRoleTitle);
+
+$jobRoleTitle.change(() => {
+  if ($jobRoleTitle.val() == 'other') {
+    $textboxForJobRoleTitle.show();
+  } else {
+    $textboxForJobRoleTitle.hide();
+  }
+});
